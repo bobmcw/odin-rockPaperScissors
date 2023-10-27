1b75697f8550c9aba7c39009c56e0577fba4b3dc
bobmcw <jasiekz2014@gmail.com>
2023-10-27 15:05:44 +0200

    created functions for computer and player input and diciding a winner

---
+++ script.js
@@ -0,0 +1,46 @@
+let answers = ['rock','paper','scissors']
+//generate computers answer
+function getComputersAnswer(){
+    let randomNumber = Math.floor(Math.random() * 3)
+    return(answers[randomNumber])
+}
+console.log(getComputersAnswer())
+//get players answer
+function getPlayersAnswer(){
+    while(true)
+    {
+        let playersAnswer = prompt("what do you pick (rock,paper,scissors)")
+
+        if(answers.includes(playersAnswer.toLowerCase()))
+        {
+            return(playersAnswer.toLowerCase())
+        }
+        else
+        {
+            console.log("you did not pick a valid option")
+            
+        }
+    }
+
+}
+// console.log(getPlayersAnswer())
+
+//decide the winner
+function DecideWinner(computerA,playerA){
+    console.log(`computers answer is ${computerA}`)
+    console.log(`players answer is ${playerA}`)
+
+    if (computerA == playerA)
+    {
+        return("draw")
+    }
+    if(computerA == "rock" && playerA == "scissors"){
+        return("computer")
+    }
+    else if(computerA == "paper" && playerA == "rock")
+    {
+        return("computer")
+    }
+}
+DecideWinner("rock","scissors")
+//game
